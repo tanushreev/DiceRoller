d9368dc2f22796ffa9a52ece361a6648758a26b0
Tanushree Varshney <varshney.tanushree@gmail.com>
2020-10-21 22:28:49 +0530

    Add a button and set the OnClickListener

--- app/src/main/java/com/tanushree/diceroller/MainActivity.kt
+++ app/src/main/java/com/tanushree/diceroller/MainActivity.kt
@@ -2,11 +2,18 @@ package com.tanushree.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bRoll: Button = findViewById(R.id.bRoll)
+        bRoll.text = "Let's Roll"
+        bRoll.setOnClickListener { Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show() }
     }
 }
