d25acea5570c7b9d771c177cd19f54f92814eb15
Tanushree Varshney <varshney.tanushree@gmail.com>
2020-12-02 17:03:35 +0530

    Add the ImageView and use support library for vector drawables

--- app/src/main/java/com/tanushree/diceroller/MainActivity.kt
+++ app/src/main/java/com/tanushree/diceroller/MainActivity.kt
@@ -3,17 +3,40 @@ package com.tanushree.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var ivDice : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ivDice = findViewById(R.id.ivDice)
+
         val bRoll: Button = findViewById(R.id.bRoll)
         bRoll.text = "Let's Roll"
-        bRoll.setOnClickListener { Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show() }
+        bRoll.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        ivDice.setImageResource(drawableResource)
     }
 }
